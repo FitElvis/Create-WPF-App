9b3b8ec38612eb9737beb8db99f698ffa0640630
Lee Royston-Hayes <Lee@LMMCSystems.com>
2023-02-21 15:44:11 +0000

    Added the first cut of the template

---
+++ Source/Templates/Create-WPF-Application/Create-WPF-Application.BusinessLogic.Contracts/Class1.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace Create_WPF_Application.BusinessLogic.Contracts
+{
+    public class Class1
+    {
+    }
+}
---
+++ Source/Templates/Create-WPF-Application/Create-WPF-Application.BusinessLogic/Class1.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace Create_WPF_Application.BusinessLogic
+{
+    public class Class1
+    {
+    }
+}
