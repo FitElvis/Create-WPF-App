255324eb7c56415fa89d1f806ac8f06aec74df00
Lee Royston-Hayes <lee@royston-hayes.com>
2023-02-23 09:32:05 +0000

    Added more of the solution structure

--- Source/Templates/Create-WPF-Application/Create-WPF-Application.BusinessLogic.Contracts/Class1.cs
+++
@@ -1,8 +0,0 @@
-using System;
-
-namespace Create_WPF_Application.BusinessLogic.Contracts
-{
-    public class Class1
-    {
-    }
-}
--- Source/Templates/Create-WPF-Application/Create-WPF-Application.BusinessLogic/Class1.cs
+++
@@ -1,8 +0,0 @@
-using System;
-
-namespace Create_WPF_Application.BusinessLogic
-{
-    public class Class1
-    {
-    }
-}
